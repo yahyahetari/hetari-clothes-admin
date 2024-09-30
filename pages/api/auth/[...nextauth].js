ccb077d0b29451fc6a73bff332dd1425b089bd71
yahyahetari <132639815+yahyahetari@users.noreply.github.com>
2024-09-30 06:58:38 +0300

    up

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,12 +1,11 @@
-import NextAuth, { getServerSession } from 'next-auth'
+import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 import { MongoDBAdapter } from '@auth/mongodb-adapter'
-import clientPromise from "@/lib/mongodb";
+import clientPromise from "@/lib/mongodb"
 
 const adminEmails = ['yahyahetari2002@gmail.com', 'yahyaalhetari5@gmail.com', 'Hazembohloly@gmail.com'];
 
 export const authOptions = {
-  secret: process.env.SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
@@ -17,15 +16,21 @@ export const authOptions = {
   callbacks: {
     session: ({ session, token, user }) => {
       if (adminEmails.includes(session?.user?.email)) {
+        session.userId = user._id;  // استخدم معرف المستخدم كمعرف للجلسة
         return session;
       } else {
         return false;
       }
     },
   },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
 };
 
 export default NextAuth(authOptions);
+
 
 export async function isAdminRequest(req, res) {
   const session = await getServerSession(req, res, authOptions);
